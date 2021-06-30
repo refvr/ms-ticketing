cddcad12b1f88bf4eac907ec6016b15cdb39b33a
s01cy <26229320+s01cy@users.noreply.github.com>
2021-06-30 22:35:47 +0200

    auth service works

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -16,8 +19,27 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
